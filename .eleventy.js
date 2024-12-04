13bf604f0fd37954221742bddae0be5a66436560
Martina Kurková <martinanoha@seznam.cz>
2024-12-04 20:52:54 +0100

    pridani_eleventy a sass modulu

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("Thumbnails");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+
+  return {
+    dir: {
+      input: ".",         // Kořenová složka jako vstup
+      output: "_site",    // Výstupní složka
+    },
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
+
+
+
+
