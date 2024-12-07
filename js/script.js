d10f9506765b1c5508cef9f29d7342ab0b31e2d0
Martina Kurková <martinanoha@seznam.cz>
2024-12-07 07:53:14 +0100

    kontakt_hotovo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const logo = document.querySelector(".header__link");
 const originalText = logo.textContent;
 
 logo.addEventListener("mouseover", ()  =>{
-    logo.textContent = "Kurková Nožičková";
+    logo.textContent = "ilustrace";
 });
 
 logo.addEventListener("mouseout", ()  => {
