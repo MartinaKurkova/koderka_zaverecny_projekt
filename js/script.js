685b446b9cdc12c79c5c62c9133c2034464750f6
Martina Kurková <martinanoha@seznam.cz>
2024-12-14 14:48:25 +0100

    snad_hotovo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 const logo = document.querySelector(".header__link");
-const originalText = logo.textContent;
+const originalText = logo.textContent; 
 
-logo.addEventListener("mouseover", ()  =>{
+function updateLastName() {
+    if (window.innerWidth > 850) {
+        logo.textContent = "Martina Kurková Nožičková";
+    } else {
+        logo.textContent = "Martina Kurková";
+    }
+}
+
+updateLastName();
+window.addEventListener("resize", updateLastName);
+
+logo.addEventListener("mouseover", () => {
     logo.textContent = "ilustrace";
 });
 
-logo.addEventListener("mouseout", ()  => {
-    logo.textContent = originalText;
+logo.addEventListener("mouseout", () => {
+    updateLastName();
 });
+
